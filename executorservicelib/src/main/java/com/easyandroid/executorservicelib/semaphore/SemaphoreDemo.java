7b32d1999afdff3a0bfd907cfe924bb021dc4a8f
gycold <583816604@qq.com>
2017-01-19 14:51:00 +0800

    Initial commit

---
+++ executorservicelib/src/main/java/com/easyandroid/executorservicelib/semaphore/SemaphoreDemo.java
@@ -0,0 +1,46 @@
+package com.easyandroid.executorservicelib.semaphore;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * package: com.easyandroid.executorservicelib.semaphore.SemaphoreDemo
+ * author: gyc
+ * description:信号量控制的示例
+ * time: create at 2017/1/19 14:39
+ */
+
+public class SemaphoreDemo {
+
+    public static void main(String[] args) {
+        //线程池
+        ExecutorService exec = Executors.newCachedThreadPool();
+        //只能5个线程同时访问
+        final Semaphore semp = new Semaphore(5);
+        //模拟20个客户端访问
+        for (int index = 0;index < 50; index++) {
+            final int NO = index;
+            Runnable run = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        //获取许可
+                        semp.acquire();
+                        System.out.println("Accessing:" + NO);
+                        Thread.sleep((long) (Math.random() * 6000));
+                        //访问完后，释放
+                        semp.release();
+                        //availablePermits()指的是当前信号灯库中有多少个可以被使用
+                        System.out.println("-----------" + semp.availablePermits());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            exec.execute(run);
+        }
+        //退出线程池
+        exec.shutdown();
+    }
+}
